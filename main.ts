a3bbc8615d9b7b3dbb0d378cc00a2333cc427e23
chickencuber <113270822+chickencuber@users.noreply.github.com>
2024-06-13 18:03:54 -0500

    added decoder

--- main.ts
+++ main.ts
@@ -22,4 +22,44 @@ function encode(string: string): Uint8Array {
   return Uint8Array.from(arr.map((v) => v.charCodeAt(0)));
 }
 
-Deno.writeFile(args[2] + ".pb", Uint8Array.from(encode(string)));
+function decode(string: string): string {
+  const arr = string.split("");
+  let begin: string;
+  let end: string;
+  if (arr.shift() === String.fromCharCode(0x01)) {
+    begin = "{";
+    end = "}";
+  } else {
+    begin = "[";
+    end = "]";
+  }
+  let str = arr
+    .join("")
+    .replaceAll(String.fromCharCode(0x02), ":" + String.fromCharCode(0x01))
+    .replaceAll(String.fromCharCode(0x03), ":{")
+    .replaceAll(String.fromCharCode(0x04), ":[")
+    .replaceAll(String.fromCharCode(0x05), "},")
+    .replaceAll(String.fromCharCode(0x06), "],");
+
+  while(/\01(\d+)(.*)/g.test(str)) {
+    str = str.replace(/\01(\d+)(.*)/g, (_, len, str:string) => {
+      const l = Number(len);
+      console.log(len, str);
+      return `"${str.slice(0, l)}"${str.slice(l, str.length)}`
+    });
+  }
+  return begin + str + end;
+}
+
+if (args[0] === "encode") {
+  Deno.writeFile(args[2] + ".pb", encode(string));
+} else if (args[0] === "decode") {
+  Deno.writeFile(
+    args[2] + ".json",
+    Uint8Array.from(
+      decode(string)
+        .split("")
+        .map((v) => v.charCodeAt(0))
+    )
+  );
+}
